a2de3a81de49ee93711b0e823836d046dfbd944d
LorepRocks <lorenajrc@gmail.com>
2022-10-31 17:49:55 -0500

    adding copy and delete actions to lambda function

--- import-service/handler.mjs
+++ import-service/handler.mjs
@@ -1,10 +1,11 @@
 'use strict';
 
-import { S3Client } from "@aws-sdk/client-s3";
+import { S3Client , PutObjectCommand , GetObjectCommand, CopyObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { parse } from 'csv-parse'
+
 const REGION = "us-east-1";
 const s3Client = new S3Client({ region: REGION });
-import { PutObjectCommand , GetObjectCommand} from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const corsConfig = {
   "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
@@ -41,31 +42,51 @@ export const importFile = async (event) => {
 }
 
 export const fileParser = async (event) => {
-  console.log('___records', event);
   const bucket = event.Records[0].s3.bucket.name;
   const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-  console.log('__bucket', bucket)
-  console.log('___key', key)
-
+  const copyKey = key.replace('uploaded','parsed')
+ 
   const params = {
     Bucket: bucket,
     Key: key
+  }
+
+  const copyParams ={
+    Bucket: bucket,
+    CopySource: bucket + '/' + key,
+    Key: copyKey,
   }
 
   try {
     // Get the object from the Amazon S3 bucket. It is returned as a ReadableStream.
     const data = await s3Client.send(new GetObjectCommand(params));
     // Convert the ReadableStream to a string.
-    const dataString = await data.Body.transformToString();
-    console.log('___dataString',dataString)
+    data.Body.pipe(parse({
+      delimiter: ','
+    })).on("data", (row) => {
+      console.log('row -->',row)
+    })
+
+    //copy file to parsed/ folder
+    const response = await s3Client.send(new CopyObjectCommand(copyParams))
+    console.log('copy response ->', response )
+
+    //deleting file from uploaded/ folder
+
+    const delResponse = await s3Client.send(new DeleteObjectCommand(params))
+    console.log('delete response ->', delResponse)
+
+    return {
+      statusCode: 200,
+      headers:corsConfig,
+      body: JSON.stringify({message: `File ${key} parsed successfully`}, null, 2)
+    }
+
   } catch (err) {
-    console.log("Error", err);
-  }
-
-
-  return {
-    statusCode: 200,
-    headers:corsConfig,
-    body: JSON.stringify({message: "FileParser"}, null, 2)
+    return {
+      statusCode: 500,
+      headers:corsConfig,
+      body: JSON.stringify({ message: `There was an error parsing file - ${err}`})
+    }
   }
 }
