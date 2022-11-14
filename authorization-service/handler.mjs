4be5da82526142714344ccaedc1ce60170a95744
LorepRocks <lorenajrc@gmail.com>
2022-11-14 10:43:56 -0500

    removing some console.log

--- authorization-service/handler.mjs
+++ authorization-service/handler.mjs
@@ -7,8 +7,6 @@ export const basicAuthorizer = async(event) => {
   const encodeAuth = Buffer.from(process.env.BASIC_AUTH_TOKEN).toString('base64');
 
   if(!headers.authorization){
-
-    console.log('__no authorization header')
     return {
       statusCode:401,
       isAuthorized: false,
@@ -21,18 +19,13 @@ export const basicAuthorizer = async(event) => {
       ),
     };
   }
-
-  console.log('___header ', headers.authorization)
-  console.log('___ encode', `Basic ${encodeAuth}`)
    
   if(headers.authorization === `Basic ${encodeAuth}`) {
-    console.log('__success')
     return {
       statusCode:200,
       isAuthorized: true
     };
   }else {
-    console.log('___are not equals')
     return {
       statusCode:403,
       isAuthorized: false,
